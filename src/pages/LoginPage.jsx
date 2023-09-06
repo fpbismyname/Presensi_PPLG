4a248944c0a6e8a02bd6acee8be4542222af9514
fpbismyname <fajarpebefajar12@gmail.com>
2023-09-06 21:13:15 +0700

    (1.0.7) Updated

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,20 +1,57 @@
 import React from "react";
 import '../styles/pages.css';
-import Icon from '../assets/react.svg';
 
-const LoginPage = () => {
-  return(
-        <div className="Card">
-            <div className="InnerCard">
-            <h1 className="titleLogin">PPLG</h1>
-            <form onSubmit={null} className="Forms">
-                <input placeholder="Username" type="text" id="User"/>
-                <input placeholder="Password" type="password" id="Pass"/>
-                <input type="submit" value="Login" id="login"/>
-            </form>
+class LoginPage extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        if (document.getElementById("User").value === "Admin" && document.getElementById("Pass").value === "Admin123") {
+            alert("Login Success");
+        } else {
+            alert("Login Failed");
+        }
+    }
+
+    render() {
+        return (
+            <div className="Card">
+                <div className="InnerCard">
+                    <h1 className="titleLogin">PPLG</h1>
+                    <form onSubmit={this.handleSubmit} className="Forms">
+                        <input placeholder="Username" type="text" id="User" />
+                        <input placeholder="Password" type="password" id="Pass" />
+                        <input type="submit" value="Login" id="login" />
+                    </form>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+// const LoginPage = () => {
+
+//     const user = document.getElementById("User");
+//     const pass = document.getElementById("Pass");
+//     constructor(props) {
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+//     handleSubmit(event)
+//     return (
+//         <div className="Card">
+//             <div className="InnerCard">
+//                 <h1 className="titleLogin">PPLG</h1>
+//                 <form onSubmit="" className="Forms">
+//                     <input placeholder="Username" type="text" id="User" />
+//                     <input placeholder="Password" type="password" id="Pass" />
+//                     <input type="submit" value="Login" id="login" />
+//                 </form>
+//             </div>
+//         </div>
+//     )
+// }
 
 export default LoginPage;
